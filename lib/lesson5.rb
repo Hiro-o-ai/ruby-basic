88d5204b99bb0b13ab739e494293e492538703a0
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-07-21 19:53:11 +0900

    start 5.3

--- lib/lesson5.rb
+++ lib/lesson5.rb
@@ -6,5 +6,41 @@ currencies = {"japan" => "yen", "us" => "dollar", "india" => "rupee"}
 currencies.each do |key_value|
   key = key_value[0]
   value = key_value[1]
-  p "#{key}:#{value}"
+  #p "#{key}:#{value}"
 end
+
+a = {"x" => 1, "y" => 2, "z" => 3 }
+
+b = {"x" => 1, "y" => 2, "z" => 3 }
+
+#p a == b
+
+c = {"z" => 3, "y" => 2, "x" => 1 }
+
+#p a == c
+
+d = {"x" => 10, "y" => 2, "z" => 3 }
+
+#p a == d
+
+#p a.size
+
+# currencies.delete("japan")
+# p currencies
+
+p currencies.delete("italy")
+p currencies.delete("italy"){ | key| "Not found: #{key}"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
