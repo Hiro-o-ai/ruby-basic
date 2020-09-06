a3ca9d51f41721ae62088010e34bd39fa8b927ef
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-09-06 17:39:50 +0900

    finish_8章

--- lib/lesson8.rb
+++ lib/lesson8.rb
@@ -63,6 +63,91 @@
 # p Product.create_products([])
 # p Product.log("hello")
 
+# NG例  moduleでインスタンスを変更している
+# module NameChanger
+#   def change_name
+#     @name = 'アリス'
+#   end
+# end
+# class User
+#   include NameChanger
+#   attr_reader :name
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+# # OK例　メソッド経由でデータを変更している
+# module NameChanger
+#   def change_name
+#     self.name = 'アリス'
+#   end
+# end
+# class User
+#   include NameChanger
+#   attr_reader :name
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# namespaceとしてのmodule
+# 同じクラス名でも問題なし
+# module Baseball
+#   class Second
+#   end
+# end
+# module Clock
+#   class Second
+#   end
+# end
+# Baseball::Second.new
+# Clock::Second.new
+# namespaceにない、同名のメソッドをmoduleないで呼び出す場合
+# ::Second.new
+
+# moduleと特異メソッド
+# module Loggable
+#   def self.log(text)
+#     puts "[LOG]#{text}"
+#   end
+# end
+# Loggable.log('hello')
+
+# module_function ミックスインや特異メソッドとして利用可
+# module Loggable
+#   def log(text)
+#     puts "[LOG] #{text}"
+#   end
+#   module_function :log 他のクラスに入るとprivateメソッドとなる
+# end
+# 特異メソッド
+# Loggable.log('hello')
+# class Product
+#   include Loggable
+#   def title
+#     log 'title is called'
+#     'A great movie'
+#   end
+# end
+# ミックスイン
+# product = Product.new
+# product.title
+
+# 引数(:〇〇)を指定しないとしたのメソッドは全てなる
+# module_function
+#   def log
+#   end
+
+
+
+
+
+
+
+
+
+
+
 
 
 
