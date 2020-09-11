f9cd71fc3900f0bbb214259bf4351879790fd32e
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-09-11 19:08:10 +0900

    start_カレンダー問題

--- parfect_ruby/chapter2.rb
+++ parfect_ruby/chapter2.rb
@@ -1,7 +1,42 @@
-def hello(names)
-  names.each do |name|
-    puts "hello, #{name.upcase}"
-  end
-end
-rubies = ['mri', 'jruby', 'rubinius']
-hello(rubies)
+# 基礎の基礎
+# メソッドの定義
+# def hello(names)
+#   names.each do |name|
+#     puts "hello, #{name.upcase}"
+#   end
+# end
+# rubies = ['mri', 'jruby', 'rubinius']
+# hello(rubies)
+
+# returnについて
+# def add(a, b)
+#   return a + b
+#   puts '表示されません'
+# end
+# p add(1, 1) 結果：2
+
+# 行をなくすには;
+# name = 'ruby' ; puts(name.upcase)
+
+# ローカル変数とグローバル変数と定数
+# foo = 'fun' 一定の範囲でのみ呼び出しできる
+# $bar = 'fun' 様々なところから呼び出しできる
+# FOO = 'fun' 再代入できるがしないようにすること
+
+# class Ruler
+#   attr_accessor :length
+#   def display_length
+#     puts length
+#   end
+# end
+# ruler = Ruler.new
+# ruler.length = 30
+# ruler.display_length
+
+
+
+
+
+
+
+
---
+++ task/carender.rb
@@ -0,0 +1,5 @@
+# カレンダー作成問題(今月)
+require 'date'
+
+data = Date.today
+p data.to_s
