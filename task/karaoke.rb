46751e39704fffe19a70acf8fd5376aebd484814
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-09-16 22:04:26 +0900

    first_karaokemachine

--- task/karaoke.rb
+++ task/karaoke.rb
@@ -1,1 +1,23 @@
 # カラオケマシン
+# scale1 = ['A', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+# scale2 = ['ド', 'レ', 'ミ', 'ミ#', 'ファ', 'ファ#', 'ソ', 'ラ', 'F#', 'G', 'G#']
+
+class KaraokeMachine
+  attr_accessor :melody
+
+  def initialize(melody)
+    @melody = melody.split('')
+  end
+
+  def transpose(count)
+    scale = ['A', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    @melody.map do |melody|
+      unless melody == ' ' && count <= scale.length
+        scale[scale.index(melody) + count]
+      end
+    end
+    puts @melody.join('')
+  end
+end
+
+
