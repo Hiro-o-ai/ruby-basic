e5dc97a85f4aaf5bf04ed68095e86072dd206ed7
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-10-10 16:03:29 +0900

    データの座標指定

--- A*/A.rb
+++ A*/A.rb
@@ -2,110 +2,138 @@
 # 出力.txtを作成できるようにする
 
 # 入力.txtのデータを呼び出し、input変数の中に1行を1つの配列として入れていく
-maze = []
+input = []
 
 File.open( "入力.txt", mode = "rt" ) {|f|
   f.each_line{|line|
-      maze << line.chomp.split('')
+      input << line.chomp.split('')
   }
 }
 
-class Node
-  attr_accessor :id, :edges, :cost, :done, :from
-  def initialize(id, edges=[], cost=nil, done=false)
-    @id, @edges, @cost, @done = id, edges, cost, done
-  end
-end
+# txtのデータに座標を入れていく
+maze = []
+y = 0
+input.each do |maze_y|
+  x = 0
+  ary = []
+  maze_y.each do |maze_x|
 
-class Edge
-  attr_reader :cost, :nid
-  def initialize(cost, nid)
-    @cost, @nid = cost, nid
-  end
-end
-
-class Graph
-  def initialize(data)
-    @nodes =
-      data.map do |id, edges|
-        edges.map! { |edge| Edge.new(*edge) }
-        Node.new(id, edges)
-      end
-  end
-
-  def cost(nid, sid)
-    dijkstra(sid)
-    @nodes.find { |node| node.id == nid }.cost
-  end
-
-  def route(sid, gid)
-    dijkstra(sid)
-    base = @nodes.find { |node| node.id == gid }
-    res = [base]
-    while base = @nodes.find { |node| node.id == base.from }
-      res << base
+    node = [maze_x, x, y]
+    ary.push(node)
+    if maze_x == "S"
+      $start = node
+    elsif maze_x == "G"
+      $goal = node
     end
-    res
-  end
 
-  def print_route(sid, gid)
-    res = route(sid, gid)
-    puts res.reverse.map { |node| "#{node.id}(#{node.cost})" }.join(" -> ")
+    x += 1
   end
-
-  private
-  def dijkstra(sid)
-    @nodes.each do |node|
-      node.cost = node.id == sid ? 0 : nil
-      node.done = false
-      node.from = nil
-    end
-    loop do
-      done_node = nil
-      @nodes.each do |node|
-        next if node.done or node.cost.nil?
-        done_node = node if done_node.nil? or node.cost < done_node.cost
-      end
-      break unless done_node
-
-      done_node.done = true
-      done_node.edges.each do |edge|
-        to = @nodes.find{ |node| node.id == edge.nid }
-        cost = done_node.cost + edge.cost
-        from = done_node.id
-        if to.cost.nil? || cost < to.cost
-          to.cost = cost
-          to.from = from
-        end
-      end
-    end
-  end
+  maze.push(ary)
+  y += 1
 end
 
+puts $start
 
-nodes = {}
-maze.each.with_index do |line, y|
-  line.each.with_index do |data, x|
-    next if data == '*'
-    id = data.match(/\w/) ? $& : "#{y}_#{x}"
-    edges =
-      [[-1, 0], [1, 0], [0, -1], [0, 1]].inject([]) do |mem, (_y, _x)|
-        _x += x; _y += y
-        case maze[_y][_x]
-        when /\w/ then mem << $&
-        when /\s/ then mem << "#{_y}_#{_x}"
-        else mem
-        end
-      end.map { |nid| [1, nid] }
-    nodes[id] = edges
-  end
-end
-g = Graph.new(nodes)
 
-route = g.route('S', 'G')
-maze.each_with_index do |line, y|
-  line.each_with_index do |data, x|
-    print route.find { |pos| pos.id == "#{y}_#{x}" } ? '$' : data
-  end
-  print "\n"
-end
+
+
+
+# class Node
+#   attr_accessor :id, :edges, :cost, :done, :from
+#   def initialize(id, edges=[], cost=nil, done=false)
+#     @id, @edges, @cost, @done = id, edges, cost, done
+#   end
+# end
+
+# class Edge
+#   attr_reader :cost, :nid
+#   def initialize(cost, nid)
+#     @cost, @nid = cost, nid
+#   end
+# end
+
+# class Graph
+#   def initialize(data)
+#     @nodes =
+#       data.map do |id, edges|
+#         edges.map! { |edge| Edge.new(*edge) }
+#         Node.new(id, edges)
+#       end
+#   end
+
+#   def cost(nid, sid)
+#     dijkstra(sid)
+#     @nodes.find { |node| node.id == nid }.cost
+#   end
+
+#   def route(sid, gid)
+#     dijkstra(sid)
+#     base = @nodes.find { |node| node.id == gid }
+#     res = [base]
+#     while base = @nodes.find { |node| node.id == base.from }
+#       res << base
+#     end
+#     res
+#   end
+
+#   def print_route(sid, gid)
+#     res = route(sid, gid)
+#     puts res.reverse.map { |node| "#{node.id}(#{node.cost})" }.join(" -> ")
+#   end
+
+#   private
+#   def dijkstra(sid)
+#     @nodes.each do |node|
+#       node.cost = node.id == sid ? 0 : nil
+#       node.done = false
+#       node.from = nil
+#     end
+#     loop do
+#       done_node = nil
+#       @nodes.each do |node|
+#         next if node.done or node.cost.nil?
+#         done_node = node if done_node.nil? or node.cost < done_node.cost
+#       end
+#       break unless done_node
+
+#       done_node.done = true
+#       done_node.edges.each do |edge|
+#         to = @nodes.find{ |node| node.id == edge.nid }
+#         cost = done_node.cost + edge.cost
+#         from = done_node.id
+#         if to.cost.nil? || cost < to.cost
+#           to.cost = cost
+#           to.from = from
+#         end
+#       end
+#     end
+#   end
+# end
+
+
+# nodes = {}
+# maze.each.with_index do |line, y|
+#   line.each.with_index do |data, x|
+#     next if data == '*'
+#     id = data.match(/\w/) ? $& : "#{y}_#{x}"
+#     edges =
+#       [[-1, 0], [1, 0], [0, -1], [0, 1]].inject([]) do |mem, (_y, _x)|
+#         _x += x; _y += y
+#         case maze[_y][_x]
+#         when /\w/ then mem << $&
+#         when /\s/ then mem << "#{_y}_#{_x}"
+#         else mem
+#         end
+#       end.map { |nid| [1, nid] }
+#     nodes[id] = edges
+#   end
+# end
+# g = Graph.new(nodes)
+
+# route = g.route('S', 'G')
+# maze.each_with_index do |line, y|
+#   line.each_with_index do |data, x|
+#     print route.find { |pos| pos.id == "#{y}_#{x}" } ? '$' : data
+#   end
+#   print "\n"
+# end
